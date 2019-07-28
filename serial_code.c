a4643e454fe9926528bf3c95e5bc1c88c762a3a1
Garrett Stonis <stonisg@wit.edu>
2019-07-28 15:23:34 -0400

    First attempt with the branch.

--- serial_code.c
+++ serial_code.c
@@ -2,16 +2,25 @@
 #include <stdlib.h>
 #include <omp.h>
 
-int main(int argc, int **argv){
-    int num_updates=5;
+int main(int argc, int **argv)
+{
+    int num_updates = 100;
+
+
+    int track_size = 20;
+
     int num_trains = 5;
     int num_stations = 4;
 
-    for(int i =0; i<num_trains;i++){
+    for(int i = 0; i < num_trains ; ++i)
+    {
         Train t = new Train();
     }
-    for(int i=0;i<num_updates;i++){
-        
+
+
+    for(int i = 0; i < num_updates; ++i)
+    {
+
 
     }
 }
