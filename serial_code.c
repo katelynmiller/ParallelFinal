faf4a7c19efd5642a197610ea7582c68d52cea12
katelynmiller <32112041+katelynmiller@users.noreply.github.com>
2019-07-28 15:16:36 -0400

    Serial Code concept
    Brain dump for creating working serial train / train station code

---
+++ serial_code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, int **argv){
+    int num_updates=5;
+    int num_trains = 5;
+    int num_stations = 4;
+
+    for(int i =0; i<num_trains;i++){
+        Train t = new Train();
+    }
+    for(int i=0;i<num_updates;i++){
+        
+
+    }
+}
