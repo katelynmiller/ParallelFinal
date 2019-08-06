a2b3fd4e9b32cd1a2888408776dda68d3126b504
Conner P. Theberge <thebergec@wit.edu>
2019-08-06 16:06:16 -0400

    Update BigWorker.c

--- src/BigWorker.c
+++ src/BigWorker.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 #include "mpi.h"
 #include <omp.h>
-
-struct Train {
-    int id;
-    int curPassengers;
-    int position;
-};
-
-struct Station {
-    int id;
-    int waitingPassengers;
-    int arrivingPassengers;
-};
+#include "simulation.h"
 
 void takeTrain(struct Train *train, struct Station *station) {
 
@@ -22,48 +11,85 @@ void takeTrain(struct Train *train, struct Station *station) {
         #pragma omp single
         {
 
-            int allPassengers = train->curPassengers;
+            int allPassengers = train->num_passengers;
             for (int i = 0; i < allPassengers; i++) {
                 #pragma omp task
                 {
 
-                    train->curPassengers--;
-                    station->arrivingPassengers++;
-                    printf("Update Train (%d) Station Waiting (%d) Station Arriving (%d)\n",train->curPassengers,station->waitingPassengers,station->arrivingPassengers);
+                    train->num_passengers--;
+                    station->passengers_arriving++;
+                    //printf("Update Train (%d) Station Waiting (%d) Station Arriving (%d)\n",train->num_passengers,station->passengers_waiting,station->passengers_arriving);
 
                 } //end task
             }
 
-            int waitingPassengers = station->waitingPassengers;
-            for (int i = 0; i < waitingPassengers; i++) {
+            int passengers_waiting = station->passengers_waiting;
+            for (int i = 0; i < passengers_waiting; i++) {
                 #pragma omp task
                 {
 
-                    train->curPassengers++;
-                    station->waitingPassengers--;
-                    printf("Update Train (%d) Station Waiting (%d) Station Arriving (%d)\n",train->curPassengers,station->waitingPassengers,station->arrivingPassengers);
+                    train->num_passengers++;
+                    station->passengers_waiting--;
+                    //printf("Update Train (%d) Station Waiting (%d) Station Arriving (%d)\n",train->num_passengers,station->passengers_waiting,station->passengers_arriving);
 
                 } // end task
             }
 
-            printf("BLEHHHHH\n");
-            station->waitingPassengers = station->arrivingPassengers;
-            station->arrivingPassengers = 0;
+            station->passengers_waiting = station->passengers_arriving;
+            station->passengers_arriving = 0;
 
         } //end single
 
     } //end parallel
 
+    int status = 0;
+
+    MPI_Send(&status,
+             1,
+             MPI_INT,
+             0,
+             0,
+             MPI_COMM_WORLD);
+
 } //end func
 
-int main(void) {
+int main(int argc, char** argv) {
 
-    struct Station station = {0, 20, 0};
-    struct Train train = {0, 10, 0};
+    int numRanks;
+    int rank;
 
-    takeTrain(&train, &station);
+    //MPI Initialization
 
-    printf("Train Passengers (from 10): %d\n", train.curPassengers);
-    printf("Station Waiting (from 20): %d\n", station.waitingPassengers);
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   
+    MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
+
+    struct Station station = {0, 200, 0};
+    struct Train train = {0, 100};
+
+    int response = 0;
+    int s = 1;
+
+    if (rank == 0) {
+
+        MPI_Recv(&response,
+                 1,
+                 MPI_INT,
+                 1,
+                 0,
+                 MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+
+    } else {
+
+        takeTrain(&train, &station);
+        printf("Train Passengers (from 10): %d\n", train.num_passengers);
+        printf("Station Waiting (from 20): %d\n", station.passengers_waiting);
+
+    }
+
+    MPI_Finalize();
 
 }
