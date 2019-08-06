a2b386a6351d83830109e0e7a8638ab674280c30
Garrett Stonis <stonisg@wit.edu>
2019-08-06 16:32:50 -0400

    Moving in serial done.

--- serial_code.c
+++ serial_code.c
@@ -52,7 +52,7 @@ int main(int argc, int **argv)
 
     int id_counter = 1;
 
-    for(int i = 0; i < num_trains; ++i)
+    for (int i = 0; i < num_trains; ++i)
     {
         int temp_position = rand() % track_size;
 
@@ -69,7 +69,7 @@ int main(int argc, int **argv)
 
     id_counter = 1;
 
-    for(int i = 0; i < num_stations; ++i)
+    for (int i = 0; i < num_stations; ++i)
     {
         int temp_position = rand() % track_size;
 
@@ -90,7 +90,7 @@ int main(int argc, int **argv)
 
     printf("Stations:\n");
 
-    for(int i = 0; i < track_size; ++i)
+    for (int i = 0; i < track_size; ++i)
     {
         printf("station_track[%d].ID = %d\n", i, station_track[i].ID);
 
@@ -104,7 +104,7 @@ int main(int argc, int **argv)
 
     printf("\n\nTrains:\n");
 
-    for(int i = 0; i < track_size; ++i)
+    for (int i = 0; i < track_size; ++i)
     {
         printf("train_track[%d].ID = %d\n", i, train_track[i].ID);
 
@@ -116,21 +116,50 @@ int main(int argc, int **argv)
     //}
 
 
-    for(int i = 0; i < num_updates; ++i)
+    for (int i = 0; i < num_updates; ++i)
     {
-        //
-    }
-
-    for(int i = 0; i < num_trains; i++)
-    {
-        if(Train.StationID == Station.ID)
+        for (int q = 0; q < track_size; ++q)
         {
-            while(Train.num_passengers != 0)
+            if (train_track[q].ID != 0)
             {
-                Train.num_passengers--;
-                printf("People on train %d",Train.num_passengers);
-                Station.passengers_arriving++;
-                printf("People on track %d",Station.passengers_arriving);
+                if (station_track[q].ID != 0)
+                {
+                    //Unloading process begins.
+                }
+                else    //Move the train if possible.
+                {
+                    int looking_position;
+
+                    if (q == track_size - 1)
+                    {
+                        looking_position = 0;
+                    }
+                    else
+                    {
+                        looking_position = q + 1;
+                    }
+
+                    if (train_track[looking_position].ID == 0)  //There is a free spot to move.
+                    {
+                        train_track[looking_position].ID = train_track[q].ID;
+                        train_track[looking_position].num_passengers = train_track[q].num_passengers;
+
+                        train_track[q].ID = 0;
+                        train_track[q].num_passengers = 0;
+                    }
+                    else
+                    {
+                        //Do nothing; it can't move.
+                    }
+                }
+
+//                while(Train.num_passengers != 0)
+//                {
+//                    Train.num_passengers--;
+//                    printf("People on train %d", Train.num_passengers);
+//                    Station.passengers_arriving++;
+//                    printf("People on track %d", Station.passengers_arriving);
+//                }
             }
         }
     }
